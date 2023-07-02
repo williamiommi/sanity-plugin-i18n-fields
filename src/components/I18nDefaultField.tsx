e76d84677e62a131366f43dcd210b2edd8fbafad
williamiommi <william.iommi@gmail.com>
2023-07-02 16:39:34 +0200

    chore: hook for setting up css vars

--- src/components/I18nDefaultField.tsx
+++ src/components/I18nDefaultField.tsx
@@ -1,11 +1,13 @@
 import {ObjectFieldProps} from 'sanity'
 import {I18nFieldsConfig} from '../types/I18nFields'
+import useSetupCssVars from '../hooks/useSetupCssVars'
 
 const I18nDefaultField = (
   props: ObjectFieldProps,
   config: I18nFieldsConfig,
   fieldType: 'string' | 'text'
 ) => {
+  useSetupCssVars()
   return <div>default field for {fieldType}</div>
 }
 
---
+++ src/hooks/useSetupCssVars.ts
@@ -0,0 +1,23 @@
+import {useTheme} from '@sanity/ui'
+import {useEffect} from 'react'
+
+// setting up css variables to use sanity/ui colors inside css style
+const useSetupCssVars = (): void => {
+  const sanityTheme = useTheme()
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      '--i18n-readOnly-warning',
+      sanityTheme.sanity.color.solid.caution.hovered.bg
+    )
+    document.documentElement.style.setProperty(
+      '--i18n-bg-selected',
+      sanityTheme.sanity.color.card.selected.bg
+    )
+    document.documentElement.style.setProperty(
+      '--i18n-bg-hover',
+      sanityTheme.sanity.color.card.hovered.bg
+    )
+  }, [])
+}
+
+export default useSetupCssVars
