bc3cb2c0b3c1233bbd11102de6c29e6212a71c0a
williamiommi <william.iommi@gmail.com>
2023-07-02 17:03:40 +0200

    chore: slider components

---
+++ src/components/Slider/Slide.tsx
@@ -0,0 +1,54 @@
+import {FormEvent} from 'react'
+import {Box, Text, Tooltip, useTheme} from '@sanity/ui'
+import {ErrorOutlineIcon} from '@sanity/icons'
+import {InternalLocale} from '../../types/Locale'
+import {I18nFieldsConfigUI} from '../../types/I18nFields'
+
+interface SlideProps {
+  name: string | undefined
+  locale: InternalLocale
+  index: number
+  isSelected: boolean
+  pluginUi: I18nFieldsConfigUI
+  onClick: (e: FormEvent<HTMLLabelElement>) => void
+}
+const Slide = ({name, locale, index, isSelected, pluginUi, onClick}: SlideProps) => {
+  const sanityTheme = useTheme()
+  return (
+    <Tooltip
+      key={locale.code}
+      content={
+        <Box padding={2}>
+          <Text muted size={0}>
+            {`${locale.title || locale.code}${locale.isReadOnly ? ' - READONLY' : ''}`}
+          </Text>
+        </Box>
+      }
+      fallbackPlacements={['right', 'left']}
+      placement="top"
+      portal
+    >
+      <label
+        id={`${name}-${locale.code}`}
+        htmlFor={`${name}.${locale.code}`}
+        className="keen-slider__slide i18n--slider-language-slide"
+        data-ui={pluginUi?.selected}
+        data-selected={isSelected}
+        data-read-only={locale.isReadOnly}
+        data-code={locale.code}
+        data-index={index}
+        onClick={onClick}
+      >
+        {locale.hasError && (
+          <ErrorOutlineIcon
+            color={sanityTheme.sanity.color.solid.critical.enabled.bg}
+            fontSize={12}
+          />
+        )}
+        <div className="-label">{locale.label}</div>
+      </label>
+    </Tooltip>
+  )
+}
+
+export default Slide
---
+++ src/components/Slider/Wrapper.tsx
@@ -0,0 +1,53 @@
+import {FormEvent, useEffect, useRef, useState} from 'react'
+import 'keen-slider/keen-slider.min.css'
+import {useKeenSlider} from 'keen-slider/react'
+import {I18nFieldsConfigUI} from '../../types/I18nFields'
+import {InternalLocale} from '../../types/Locale'
+import Slide from './Slide'
+
+interface WrapperProps {
+  name: string | undefined
+  pluginUi: I18nFieldsConfigUI
+  locales: InternalLocale[]
+  activeLocale: InternalLocale
+  onClick: (e: FormEvent<HTMLLabelElement>) => void
+}
+const Wrapper = ({name, pluginUi, locales, activeLocale, onClick}: WrapperProps) => {
+  const [ready, setReady] = useState(false)
+  const sliderRef = useRef(null)
+  const [keenRef] = useKeenSlider(
+    {
+      mode: 'free',
+      slides: {perView: 'auto'},
+    },
+    []
+  )
+
+  useEffect(() => {
+    keenRef(sliderRef.current)
+    setReady(true)
+  }, [keenRef])
+
+  return (
+    <div
+      ref={sliderRef}
+      className="keen-slider i18n--slider-language"
+      data-ready={ready}
+      data-position={pluginUi?.position}
+    >
+      {locales.map((locale, index) => (
+        <Slide
+          key={locale.code}
+          index={index}
+          name={name}
+          isSelected={activeLocale.code === locale.code}
+          locale={locale}
+          pluginUi={pluginUi}
+          onClick={onClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Wrapper
