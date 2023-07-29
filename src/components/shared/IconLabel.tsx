540bca2a31b311e09301ffd5cc7e807a1fc7bebb
williamiommi <william.iommi@gmail.com>
2023-07-29 18:56:48 +0200

    chore: minor on unknown label

--- src/components/shared/IconLabel.tsx
+++ src/components/shared/IconLabel.tsx
@@ -1,5 +1,6 @@
 import {ComponentType, ReactNode, createElement, isValidElement} from 'react'
 import {isValidElementType} from 'react-is'
+import {UnknownIcon} from '@sanity/icons'
 
 interface IconLabelProps {
   label: ReactNode | ComponentType
@@ -8,7 +9,7 @@ interface IconLabelProps {
 const IconLabel = ({label}: IconLabelProps) => {
   if (isValidElement(label) || typeof label === 'string' || typeof label === 'number') return label
   if (isValidElementType(label)) return createElement(label)
-  return 'something went wrong'
+  return <UnknownIcon fontSize={20} />
 }
 
 export default IconLabel
